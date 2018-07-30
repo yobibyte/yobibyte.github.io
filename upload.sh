f86ccdfc5a2c8c8293c044d6bbacad356f2d6e24
yobibyte <vitaliykurin@gmail.com>
2018-07-30 16:09:49 +0300

    fix upload script

--- upload.sh
+++ upload.sh
@@ -1,8 +1,9 @@
-git checkout inner #just to be sure
-git add -A
-git commit -m 'update'
-git push origin inner
 
 pelican content
 ghp-import output -b master
 git push origin master
+
+git checkout inner #just to be sure
+git add -A
+git commit -m 'update'
+git push origin inner
