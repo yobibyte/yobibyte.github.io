74624273e318e33c41972cc034abee5105b60093
yobibyte <vitaliykurin@gmail.com>
2021-02-17 22:01:12 +0000

    update

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,7 +35,7 @@ SOCIAL = (
 
 MAIN_MENU = True
 LINKS = (
-    ("Notes", "https://www.notion.so/Paper-Notes-by-Vitaly-Kurin-97827e14e5cd4183815cfe3a5ecf2f4c"),
+    ("Paper Notes", "https://www.notion.so/Paper-Notes-by-Vitaly-Kurin-97827e14e5cd4183815cfe3a5ecf2f4c"),
 )
 
 DISQUS_SITENAME = 'yobiblog-1'
