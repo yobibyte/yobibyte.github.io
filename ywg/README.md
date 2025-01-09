575b65b65b2f9f6525a92cf4996b8aa7232febfd
yobibyte <y0b1byte@proton.me>
2025-01-09 23:04:38 +0000

    remove ywg

--- ywg/README.md
+++
@@ -1,3 +0,0 @@
-ywg stands for yobibyte's website generator. 
-
-This directory contains python code I use to simplify managing my personal website.
--- ywg/util.py
+++
@@ -1,7 +0,0 @@
-import email.utils
-
-def get_pubdate():
-    return email.utils.formatdate()
-
-if __name__ == '__main__':
-    print(get_pubdate())
