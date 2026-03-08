9882735403572ea88d2c3dd169f857d76895dd97
yobibyte <y0b1byte@proton.me>
2026-03-08 10:38:34 +0000

    Platform independent ISO date.

--- tweet.sh
+++ tweet.sh
@@ -1,6 +1,6 @@
 sed -i '$d' feed.xml
 echo "<item><title>What&apos;s up?</title><link>https://yobibyte.github.io/whatsup.html</link><description><![CDATA[What's up?]]></description><guid>https://yobibyte.github.io/whatsup.html</guid><pubDate>" >> feed.xml
-echo "$(date -R)" >> feed.xml
+echo $(date +"%a, %d %b %Y %H:%M:%S %z") >> feed.xml
 echo "</pubDate><content:encoded><![CDATA[" >> feed.xml
 echo "<h1>What's up?</h1>" >> feed.xml
 echo "<h3>$(date)</h3>" >> feed.xml
